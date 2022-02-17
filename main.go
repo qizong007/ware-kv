ff86d5c884712f12d67d9f75d3b15f742a51c162
qizong007 <978451432@qq.com>
2022-02-17 23:30:22 +0800

    feat: test shell && server

--- main.go
+++ main.go
@@ -1,33 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/gookit/color"
-	"ware-kv/warekv/handler"
-)
-
 const (
 	Port = "7777"
 )
 
-func boot() *gin.Engine {
-	engine := gin.Default()
-	handler.Register(engine)
-	showFrame()
-	return engine
-}
-
-func showFrame() {
-	color.HiYellow.Println("                                   __")
-	color.HiYellow.Println("   _      ______ _________        / /___   __")
-	color.HiGreen.Println("  | | /| / / __ `/ ___/ _ \\______/ //_/ | / /")
-	color.HiCyan.Println("  | |/ |/ / /_/ / /  /  __/_____/ ,<  | |/ /")
-	color.HiBlue.Print("  |__/|__/\\__,_/_/   \\___/     /_/|_| |___/")
-	color.HiMagenta.Println("         😎version_0.0.1@qizong007")
-}
-
 func main() {
-	r := boot()
-	r.Run(fmt.Sprintf(":%s", Port))
+	Boot(Port)
 }
---
+++ server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gookit/color"
+	"ware-kv/warekv/handler"
+	"ware-kv/warekv/storage"
+)
+
+var Server *WareKV
+
+type WareKV struct {
+	wTable *storage.WareTable
+	router *gin.Engine
+	//options（布隆开关、日志开关）
+	//stats
+	//machine info
+	// closer
+	//tracker(AOF) head|k|v|crc32 mmap
+	// camera(RDB)
+	// GC
+	// 热点采样
+}
+
+func Boot(port string) {
+	Server = &WareKV{
+		wTable: storage.GetWareTable(),
+		router: gin.Default(),
+	}
+	handler.Register(Server.router)
+	showFrame()
+	Server.router.Run(fmt.Sprintf(":%s", port))
+}
+
+func showFrame() {
+	color.HiYellow.Println("                                   __")
+	color.HiYellow.Println("   _      ______ _________        / /___   __")
+	color.HiGreen.Println("  | | /| / / __ `/ ___/ _ \\______/ //_/ | / /")
+	color.HiCyan.Println("  | |/ |/ / /_/ / /  /  __/_____/ ,<  | |/ /")
+	color.HiBlue.Print("  |__/|__/\\__,_/_/   \\___/     /_/|_| |___/")
+	color.HiMagenta.Println("         😎version_0.0.1@qizong007")
+}
