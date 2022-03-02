c12dee535db8ede5275b541451f9b0df32e1cbcf
qizong007 <978451432@qq.com>
2022-03-02 09:57:36 +0800

    delete: test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST http://localhost:7777/str -H 'Content-Type: application/json' -d '{"k": "name","v": "qizong007"}'
-sleep 1
-curl -i http://localhost:7777/str/name
-sleep 1
-curl -X DELETE http://localhost:7777/str/name
-sleep 1
-curl -i http://localhost:7777/str/name
