9fe90d3cbe9dbea1ebaf7dca40fa441390ea7ccc
qizong007 <978451432@qq.com>
2022-03-23 23:45:54 +0800

    delete: unlock in PUT

--- handler/router.go
+++ handler/router.go
@@ -101,7 +101,6 @@ func Register(r *gin.Engine) {
 	r.POST("/lock/:key", Lock)
 	r.PUT("/lock/:key", Lock)
 	r.POST("/unlock/:key", Unlock)
-	r.PUT("/unlock/:key", Unlock)
 
 	// subscribe
 	r.POST("/subscribe", SubscribeKey)
